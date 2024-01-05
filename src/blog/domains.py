4ca4d7c0ce21c0c454003e152814b25bd220a5f5
Anton Shcherbak <shcherbak.a@selectel.ru>
2024-01-05 23:52:24 +0500

    feat: new blog project

---
+++ src/blog/domains.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    """Обычный пользователь"""
+    id: str
+
+
+@dataclass
+class Admin(User):
+    """Пользователь, наделенный правами администратора"""
+    username: str
+    password: str
+
+
+@dataclass
+class Article:
+    """Сущность статьи"""
+    id: str
+    title: str
+    content: str
--- src/blog/main.py
+++ src/blog/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from blog.resources import router
+
 
 def get_app():
     app = FastAPI()
+
+    app.include_router(router)
 
     return app
 
